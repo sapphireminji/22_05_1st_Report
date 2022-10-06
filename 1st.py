fc9e3659b35e06cce1703b869d3549b65bf9ac6b
sapphireminji <112676936+sapphireminji@users.noreply.github.com>
2022-10-06 20:49:19 +0900

    Add files via upload

---
+++ 1st.py
@@ -0,0 +1,14 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import turtle
+>>> t = turtle.Turtle()
+>>> t.shape("turtle")
+>>> t.forward(100)
+>>> t.left(90)
+>>> t.forward(100)
+>>> t.right(90)
+>>> t.forward(100)
+>>> t.right(90)
+>>> t.forward(100)
+>>> t.left(90)
+>>> t.forward(100)
